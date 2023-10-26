df40240bfd372ae74af4b4692b20fe9dc8d5f7ce
github_julijaand <julijaand@yahoo.com>
2023-10-26 14:20:22 +0300

    instructions added

--- get_global_variables.py
+++ get_global_variables.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3.8
+
 import os
 
 variables = {
-    "region name": os.environ.get("AWS_DEFAULT_REGION", "N/A"),
-    "AZ name": os.environ.get("AWS_AVAILABILITY_ZONE", "N/A"),
-    "public IP address": os.environ.get("PUBLIC_IP", "N/A"),
-    "private IP address": os.environ.get("PRIVATE_IP", "N/A"),
-    "name of instance": os.environ.get("AWS_INSTANCE_ID", "N/A"),
-    "instance ID": os.environ.get("AWS_INSTANCE_ID", "N/A"),
-    "account number": os.environ.get("AWS_ACCOUNT_ID", "N/A"),
+    "region name": os.environ.get("AWS_DEFAULT_REGION"),
+    "AZ name": os.environ.get("AWS_AVAILABILITY_ZONE"),
+    "public IP address": os.environ.get("PUBLIC_IP"),
+    "private IP address": os.environ.get("PRIVATE_IP"),
+    "name of instance": os.environ.get("AWS_INSTANCE_ID"),
+    "instance ID": os.environ.get("AWS_INSTANCE_ID"),
+    "account number": os.environ.get("AWS_ACCOUNT_ID"),
 }
 
 output_file = "/opt/output.txt"
--- get_global_variables.sh
+++ get_global_variables.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+IMDSv2_TOKEN=$(curl -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" http://169.254.169.254/latest/api/token)
+
 # Retrieve AWS instance metadata using IMDSv2
-region=$(curl -s -H "X-aws-ec2-metadata-token: REPLACE_WITH_YOUR_IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/placement/region)
-az=$(curl -s -H "X-aws-ec2-metadata-token: REPLACE_WITH_YOUR_IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)
-public_ip=$(curl -s -H "X-aws-ec2-metadata-token: REPLACE_WITH_YOUR_IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
-private_ip=$(curl -s -H "X-aws-ec2-metadata-token: REPLACE_WITH_YOUR_IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
-instance_id=$(curl -s -H "X-aws-ec2-metadata-token: REPLACE_WITH_YOUR_IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
-account_number=$(curl -s -H "X-aws-ec2-metadata-token: REPLACE_WITH_YOUR_IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/identity-credentials/ec2/info | jq -r .AccountId)
+region=$(curl -s -H "X-aws-ec2-metadata-token: $IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/placement/region)
+az=$(curl -s -H "X-aws-ec2-metadata-token: $IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)
+public_ip=$(curl -s -H "X-aws-ec2-metadata-token: $IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+private_ip=$(curl -s -H "X-aws-ec2-metadata-token: $IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+instance_id=$(curl -s -H "X-aws-ec2-metadata-token: $IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+account_number=$(curl -s -H "X-aws-ec2-metadata-token: $IMDSv2_TOKEN" http://169.254.169.254/latest/meta-data/identity-credentials/ec2/info | jq -r .AccountId)
 
 # Write the variables and their values to the output file
 output_file="/opt/output.txt"
@@ -17,4 +19,3 @@ echo "private IP address: $private_ip" >> "$output_file"
 echo "name of instance: $instance_id" >> "$output_file"
 echo "instance ID: $instance_id" >> "$output_file"
 echo "account number: $account_number" >> "$output_file"
-
